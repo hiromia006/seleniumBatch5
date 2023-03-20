b211331610b140a16b15520e21aa2fa832caf46a
Hiro Mia <hiromia006@gmail.com>
2023-03-20 22:40:50 +0600

    Firefox Browser launch

--- src/test/java/dev/selenium/test/BrowserLaunch.java
+++ src/test/java/dev/selenium/test/BrowserLaunch.java
@@ -2,7 +2,10 @@ package dev.selenium.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 
 public class BrowserLaunch {
 
@@ -12,6 +15,7 @@ public class BrowserLaunch {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.selenium.dev/");
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
